0e3ec28b47a405b1026d1ff6ba40675c70cbdd75
rinrin24 <rin.nkmr42@gmail.com>
2022-12-15 12:25:49 +0900

    feat: べき乗を計算するメソッドの追加
    べき乗を計算する**メソッドをオーバーライド。
    またレポート用のサンプルの出力を追加。
    それぞれのメソッド内の不要な条件式を削除

--- lesson9/sample4.rb
+++ lesson9/sample4.rb
@@ -11,37 +11,36 @@ class ComplexNumber
 	end;
 	def +(newNumber)
 		newComplexNumber = to_complexNumber(newNumber);
-		if(newComplexNumber.instance_of?(ComplexNumber))
-			newRealNumber = newComplexNumber.realNumber + @realNumber;
-			newImaginaryNumber = newComplexNumber.imaginaryNumber + @imaginaryNumber;
-			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
-		end;
+		newRealNumber = newComplexNumber.realNumber + @realNumber;
+		newImaginaryNumber = newComplexNumber.imaginaryNumber + @imaginaryNumber;
+		return ComplexNumber.new(newRealNumber, newImaginaryNumber);
 	end;
 	def -(newNumber)
 		newComplexNumber = to_complexNumber(newNumber);
-		if(newComplexNumber.instance_of?(ComplexNumber))
-			newRealNumber = -newComplexNumber.realNumber + @realNumber;
-			newImaginaryNumber = -newComplexNumber.imaginaryNumber + @imaginaryNumber;
-			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
-		end;
+		newRealNumber = -newComplexNumber.realNumber + @realNumber;
+		newImaginaryNumber = -newComplexNumber.imaginaryNumber + @imaginaryNumber;
+		return ComplexNumber.new(newRealNumber, newImaginaryNumber);
 	end;
 	def *(newNumber)
 		newComplexNumber = to_complexNumber(newNumber);
-		if(newComplexNumber.instance_of?(ComplexNumber))
-			newRealNumber = newComplexNumber.realNumber * @realNumber - newComplexNumber.imaginaryNumber * @imaginaryNumber;
-			newImaginaryNumber = newComplexNumber.realNumber * @imaginaryNumber + newComplexNumber.imaginaryNumber * @realNumber;
-			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
-		end;
+		newRealNumber = newComplexNumber.realNumber * @realNumber - newComplexNumber.imaginaryNumber * @imaginaryNumber;
+		newImaginaryNumber = newComplexNumber.realNumber * @imaginaryNumber + newComplexNumber.imaginaryNumber * @realNumber;
+		return ComplexNumber.new(newRealNumber, newImaginaryNumber);
 	end;
 	def /(newNumber)
 		newComplexNumber = to_complexNumber(newNumber);
-		if(newComplexNumber.instance_of?(ComplexNumber))
-			bunbo = newComplexNumber.realNumber**2 - newComplexNumber.imaginaryNumber**2
-			bunshi = self * ComplexNumber.new(newComplexNumber.realNumber, -newComplexNumber.imaginaryNumber)
-			newRealNumber = 1.0 * bunshi.realNumber / bunbo
-			newImaginaryNumber = 1.0 * bunshi.imaginaryNumber / bunbo
-			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
-		end;
+		bunbo = newComplexNumber.realNumber**2 - newComplexNumber.imaginaryNumber**2
+		bunshi = self * ComplexNumber.new(newComplexNumber.realNumber, -newComplexNumber.imaginaryNumber)
+		newRealNumber = 1.0 * bunshi.realNumber / bunbo
+		newImaginaryNumber = 1.0 * bunshi.imaginaryNumber / bunbo
+		return ComplexNumber.new(newRealNumber, newImaginaryNumber);
+	end;
+	def **(power)
+		outputNumber = self;
+		(power-1).times do
+			outputNumber = outputNumber * self;
+		end
+		return outputNumber;
 	end;
 	def inspect()
 		if(@imaginaryNumber == 0) then; return @realNumber; end;
@@ -56,15 +55,35 @@ class ComplexNumber
 end;
 
 complexNumber = ComplexNumber.new(10, 5);
-p(complexNumber)
-p(complexNumber.realNumber)
-p(complexNumber.imaginaryNumber)
+p(complexNumber);
+p(complexNumber.realNumber);
+p(complexNumber.imaginaryNumber);
 otherComplexNumber = ComplexNumber.new(20, 10);
-complexNumber = complexNumber + 5
-p(complexNumber)
-complexNumber = complexNumber + 5.5
-p(complexNumber)
-complexNumber = complexNumber + otherComplexNumber
-p(complexNumber)
-complexNumber = complexNumber - ComplexNumber.new(0, 15)
-p(complexNumber)
+complexNumber = complexNumber + 5;
+p(complexNumber);
+complexNumber = complexNumber + 5.5;
+p(complexNumber);
+complexNumber = complexNumber + otherComplexNumber;
+p(complexNumber);
+complexNumber = complexNumber - 10;
+p(complexNumber);
+complexNumber = complexNumber - 5.5;
+p(complexNumber);
+complexNumber = complexNumber - otherComplexNumber;
+p(complexNumber);
+complexNumber = complexNumber * 2
+p(complexNumber);
+complexNumber = complexNumber * 1.25
+p(complexNumber);
+complexNumber = complexNumber * otherComplexNumber;
+p(complexNumber);
+complexNumber = complexNumber / 2;
+p(complexNumber);
+complexNumber = complexNumber / 2.5;
+p(complexNumber);
+complexNumber = complexNumber / otherComplexNumber;
+p(complexNumber);
+complexNumber = ComplexNumber.new(10, 10);
+p(complexNumber);
+complexNumber = complexNumber**3;
+p(complexNumber);
