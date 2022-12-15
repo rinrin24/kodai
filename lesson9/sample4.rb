e73d746e82ba1242ec797e22743917a55fe8212a
rinrin24 <rin.nkmr42@gmail.com>
2022-12-15 11:02:14 +0900

    feat: p()メソッドでComplexNumberクラスを出力するときの形を整形
    p()メソッドでComplexNumberクラスを出力するときに、a + b * iの形式で表示するように変更

--- lesson9/sample4.rb
+++ lesson9/sample4.rb
@@ -9,64 +9,32 @@ class ComplexNumber
 	def imaginaryNumber
 		return @imaginaryNumber;
 	end;
-	def +(newComplexNumber)
-		if(newComplexNumber.instance_of?(Integer))
-			newRealNumber = newComplexNumber + @realNumber;
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
-		if(newComplexNumber.instance_of?(Float))
-			newRealNumber = newComplexNumber + @realNumber;
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
+	def +(newNumber)
+		newComplexNumber = to_complexNumber(newNumber);
 		if(newComplexNumber.instance_of?(ComplexNumber))
 			newRealNumber = newComplexNumber.realNumber + @realNumber;
 			newImaginaryNumber = newComplexNumber.imaginaryNumber + @imaginaryNumber;
 			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
 		end;
 	end;
-	def -(newComplexNumber)
-		if(newComplexNumber.instance_of?(Integer))
-			newRealNumber = -newComplexNumber + @realNumber;
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
-		if(newComplexNumber.instance_of?(Float))
-			newRealNumber = -newComplexNumber + @realNumber;
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
+	def -(newNumber)
+		newComplexNumber = to_complexNumber(newNumber);
 		if(newComplexNumber.instance_of?(ComplexNumber))
 			newRealNumber = -newComplexNumber.realNumber + @realNumber;
 			newImaginaryNumber = -newComplexNumber.imaginaryNumber + @imaginaryNumber;
 			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
 		end;
 	end;
-	def *(newComplexNumber)
-		if(newComplexNumber.instance_of?(Integer))
-			newRealNumber = newComplexNumber + @realNumber;
-			newImaginaryNumber = newComplexNumber * @imaginaryNumber
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
-		if(newComplexNumber.instance_of?(Float))
-			newRealNumber = newComplexNumber * @realNumber;
-			newImaginaryNumber = newComplexNumber * @imaginaryNumber
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
+	def *(newNumber)
+		newComplexNumber = to_complexNumber(newNumber);
 		if(newComplexNumber.instance_of?(ComplexNumber))
 			newRealNumber = newComplexNumber.realNumber * @realNumber - newComplexNumber.imaginaryNumber * @imaginaryNumber;
 			newImaginaryNumber = newComplexNumber.realNumber * @imaginaryNumber + newComplexNumber.imaginaryNumber * @realNumber;
 			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
 		end;
 	end;
-	def /(newComplexNumber)
-		if(newComplexNumber.instance_of?(Integer))
-			newRealNumber = 1.0 * @realNumber / newComplexNumber;
-			newImaginaryNumber = 1.0 * @imaginaryNumber / newComplexNumber;
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
-		if(newComplexNumber.instance_of?(Float))
-			newRealNumber = 1.0 * @realNumber / newComplexNumber;
-			newImaginaryNumber = 1.0 * @imaginaryNumber / newComplexNumber;
-			return ComplexNumber.new(newRealNumber, @imaginaryNumber);
-		end;
+	def /(newNumber)
+		newComplexNumber = to_complexNumber(newNumber);
 		if(newComplexNumber.instance_of?(ComplexNumber))
 			bunbo = newComplexNumber.realNumber**2 - newComplexNumber.imaginaryNumber**2
 			bunshi = self * ComplexNumber.new(newComplexNumber.realNumber, -newComplexNumber.imaginaryNumber)
@@ -74,6 +42,16 @@ class ComplexNumber
 			newImaginaryNumber = 1.0 * bunshi.imaginaryNumber / bunbo
 			return ComplexNumber.new(newRealNumber, newImaginaryNumber);
 		end;
+	end;
+	def inspect()
+		if(@imaginaryNumber == 0) then; return @realNumber; end;
+		if(@realNumber == 0) then; return @imaginaryNumber.to_s + " * i"; end;
+		return @realNumber.to_s + " + " + @imaginaryNumber.to_s + " * i"
+	end
+	private
+	def to_complexNumber(newNumber)
+		if(newNumber.instance_of?(ComplexNumber)) then; return newNumber; end;
+		return ComplexNumber.new(newNumber, 0);
 	end;
 end;
 
@@ -87,4 +65,6 @@ p(complexNumber)
 complexNumber = complexNumber + 5.5
 p(complexNumber)
 complexNumber = complexNumber + otherComplexNumber
+p(complexNumber)
+complexNumber = complexNumber - ComplexNumber.new(0, 15)
 p(complexNumber)
